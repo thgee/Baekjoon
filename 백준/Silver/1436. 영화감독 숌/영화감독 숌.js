e7eb1970687378fd265d5e71c45419558f372ae6
thgee <102576089+thgee@users.noreply.github.com>
2024-02-02 12:50:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 568 ms, Memory: 23356 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,28 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+// -------------------------------------------------------------------------
+
+let n = Number(input);
+
+const isTreeSix = (num) => {
+  num = String(num);
+  let cnt = 0;
+  for (let x of num) {
+    if (x === "6") cnt++;
+    else cnt = 0;
+    if (cnt === 3) return true;
+  }
+  return false;
+};
+
+let cnt = 0,
+  num = 665;
+
+while (cnt < n) {
+  num++;
+  if (isTreeSix(num)) cnt++;
+}
+
+console.log(num);
