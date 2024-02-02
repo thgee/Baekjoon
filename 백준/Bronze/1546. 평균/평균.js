c55e8ca487050fa69e62da7a17207898f59d2f97
thgee <102576089+thgee@users.noreply.github.com>
2024-02-02 12:59:45 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 9636 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [n, arr] = input;
+arr = arr.split(" ").map((it) => parseInt(it));
+// -------------------------------------------------------------------------
+
+let M = Math.max(...arr);
+console.log(
+  arr.map((it) => (it / M) * 100).reduce((acc, cur) => acc + cur) / arr.length
+);
