dc44639babc55391eb20d0a0bf8701e50b868952
thgee <102576089+thgee@users.noreply.github.com>
2024-02-03 23:40:24 +0900

    [Silver II] Title: 스택 수열, Time: 280 ms, Memory: 39044 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -1,35 +1,3 @@
-// let fs = require("fs");
-// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-// let input = fs.readFileSync(filePath).toString().trim().split("\n");
-// let [n, ...arr] = input;
-// n = Number(n);
-// arr = arr.map((it) => Number(it.trim()));
-// // -------------------------------------------------------------------------
-
-// // 1. arr을 순회하면서 1~n 중에 arr의 원소와 같은 원소가 등장할 때 까지 push
-// // 2. 같은 원소가 등장하면 다른 원소가 나올 때 까지 arr.shift()와 stack.pop()을 동시에 진행
-// // 3. 1, 2번을 계속 반복
-
-// let stack = [];
-// let res = [];
-// let i = 0,
-//   j = 0;
-// while (i < n) {
-//   while (arr[j] !== i && i < n) {
-//     stack.push(++i);
-//     res.push("+");
-//   }
-//   while (arr[j] === stack[stack.length - 1] && stack.length !== 0) {
-//     j++;
-//     stack.pop();
-//     res.push("-");
-//   }
-// }
-
-// if (stack.length === 0) {
-//   for (let x of res) console.log(x);
-// } else console.log("NO");
-
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -43,21 +11,21 @@ arr = arr.map((it) => Number(it.trim()));
 // 3. 1, 2번을 계속 반복
 
 let stack = [];
-let res = "";
+let res = [];
 let i = 0,
   j = 0;
 while (i < n) {
   while (arr[j] !== i && i < n) {
     stack.push(++i);
-    res += "+";
+    res.push("+");
   }
   while (arr[j] === stack[stack.length - 1] && stack.length !== 0) {
     j++;
     stack.pop();
-    res += "-";
+    res.push("-");
   }
 }
 
 if (stack.length === 0) {
-  console.log(res.split("").join("\n"));
+  console.log(res.join("\n"));
 } else console.log("NO");
