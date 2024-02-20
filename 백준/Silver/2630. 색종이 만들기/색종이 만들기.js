689c63f864cdb891658f78bac1e730ee3f81ec9e
thgee <102576089+thgee@users.noreply.github.com>
2024-02-21 03:32:13 +0900

    Update 색종이 만들기.js

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
@@ -23,14 +23,15 @@ const DFS = (x, y, n) => {
     tmp.push(DFS(x + dx[i] * (n / 2), y + dy[i] * (n / 2), n / 2));
   }
   let sum = tmp.reduce((acc, cur) => acc + cur, 0);
-  if (sum === 4) return 1;
-  else if (sum === 0) return 0;
+  if (sum === 4) return 1; // 4개로 나눠진 종이가 모두 파란색인 경우
+  else if (sum === 0) return 0; // 4개로 나눠진 종이가 모두 흰색인 경우
   else {
+    // 색이 섞여있는 경우 --> 4개의 종이중에 파란색이나 흰색이 있다면 카운트해줌
     tmp.map((it) => {
       if (it === 1) blueCnt++;
       else if (it === 0) whiteCnt++;
     });
-    return 100;
+    return 100; // 섞여있는 색은 합쳐진 종이에서 흰색이나 검은색으로 취급하지 않기 위해 100이라는 큰수로 예외처리
   }
 };
 
