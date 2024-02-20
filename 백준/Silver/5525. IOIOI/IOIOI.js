38d6504917ae3c9d5bcea3f71d28ed2d2bb2fca7
thgee <102576089+thgee@users.noreply.github.com>
2024-02-21 05:03:04 +0900

    [Silver I] Title: IOIOI, Time: 312 ms, Memory: 51912 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -1,10 +1,8 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let arr = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let n = Number(arr.shift());
-let m = Number(arr.shift());
-arr = arr.shift().trim();
+let [n, m, arr] = input;
 
 let target = "";
 
@@ -12,11 +10,17 @@ for (let i = 0; i < n; i++) {
   target += "IO";
   if (i === n - 1) target += "I";
 }
-let cnt = 0;
-let i = 0;
-while (1) {
-  i = arr.indexOf(target, i);
-  if (i++ === -1) break;
-  cnt++;
+
+let res = 0;
+let cnt = 1;
+for (let i = 0; i < arr.length - 2; i++) {
+  if (arr.slice(i, i + 3) === "IOI") {
+    i++;
+    cnt += 2;
+  } else {
+    if (cnt >= target.length) res += (cnt - target.length) / 2 + 1;
+    cnt = 1;
+  }
 }
-console.log(cnt);
+if (cnt >= target.length) res += (cnt - target.length) / 2 + 1;
+console.log(res);
