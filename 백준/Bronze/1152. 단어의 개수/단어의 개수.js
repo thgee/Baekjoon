6cb0eb1dc2b9bce854998119f7d186a398be8729
thgee <102576089+thgee@users.noreply.github.com>
2024-02-21 23:41:54 +0900

    [Bronze II] Title: 단어의 개수, Time: 152 ms, Memory: 18112 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,4 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let arr = fs.readFileSync(filePath).toString().trim().split(" ");
+console.log(arr[0] !== "" ? arr.length : 0);
