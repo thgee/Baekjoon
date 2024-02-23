a3f129fd4c3311c0db38968458f84918457d51be
thgee <102576089+thgee@users.noreply.github.com>
2024-02-23 12:29:32 +0900

    Update 테트로미노.js

--- 백준/Gold/14500. 테트로미노/테트로미노.js
+++ 백준/Gold/14500. 테트로미노/테트로미노.js
@@ -1,188 +1,44 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let arr = fs.readFileSync(filePath).toString().trim().split("\n");
-arr = arr.map((it) => it.split(" ").map((it) => Number(it)));
-let [n, m] = arr.shift();
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+input = input.map((it) => it.split(" ").map((it) => Number(it)));
+let [[n, m], ...arr] = input;
 
-let blocks = [
-  // o o o o
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-  ],
-  // o
-  // o
-  // o
-  // o
-  [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-  ],
-  // o o
-  // o o
-  [
-    [0, 0],
-    [1, 0],
-    [0, 1],
-    [1, 1],
-  ],
-  // o
-  // o
-  // o o
-  [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [2, 1],
-  ],
-  //   o
-  //   o
-  // o o
-  [
-    [0, 1],
-    [1, 1],
-    [2, 1],
-    [2, 0],
-  ],
-  //     o
-  // o o o
-  [
-    [0, 2],
-    [1, 2],
-    [1, 1],
-    [1, 0],
-  ],
-  // o
-  // o o o
-  [
-    [0, 0],
-    [1, 2],
-    [1, 1],
-    [1, 0],
-  ],
-  // o o
-  //   o
-  //   o
-  [
-    [0, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ],
-  // o o
-  // o
-  // o
-  [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [2, 0],
-  ],
-  // o o o
-  // o
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [1, 0],
-  ],
-  // o o o
-  //     o
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [1, 2],
-  ],
-  // o o
-  //   o o
-  [
-    [0, 0],
-    [0, 1],
-    [1, 1],
-    [1, 2],
-  ],
-  //   o o
-  // o o
-  [
-    [0, 1],
-    [0, 2],
-    [1, 0],
-    [1, 1],
-  ],
-  //   o
-  // o o
-  // o
-  [
-    [0, 1],
-    [1, 0],
-    [1, 1],
-    [2, 0],
-  ],
-  // o
-  // o o
-  //   o
-  [
-    [0, 0],
-    [1, 0],
-    [1, 1],
-    [2, 1],
-  ],
-  //   o
-  // o o o
-  [
-    [0, 1],
-    [1, 0],
-    [1, 1],
-    [1, 2],
-  ],
-  //   o
-  // o o
-  //   o
-  [
-    [0, 1],
-    [1, 0],
-    [1, 1],
-    [2, 1],
-  ],
-  // o o o
-  //   o
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [1, 1],
-  ],
-  // o
-  // o o
-  // o
-  [
-    [0, 0],
-    [1, 0],
-    [1, 1],
-    [2, 0],
-  ],
+let offset = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
 ];
 
 let max = 0;
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    let cnt = 0;
-    for (let b of blocks) {
-      let sum = 0;
-      for (let k = 0; k < 4; k++) {
-        let [nx, ny] = [i + b[k][0], j + b[k][1]];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= m) break;
-        sum += arr[nx][ny];
-      }
-      max = Math.max(max, sum);
-    }
+let isVisit = Array.from({ length: n }, () => Array(m).fill(false));
+const DFS = (x, y, l, sum) => {
+  if (l === 4) {
+    // 테트로미노 하나 완성
+    max = Math.max(max, sum);
+    return;
   }
-}
+
+  for (let [dx, dy] of offset) {
+    let nx = x + dx,
+      ny = y + dy;
+    if (nx < 0 || nx >= n || ny < 0 || ny >= m || isVisit[nx][ny]) continue;
+
+    isVisit[nx][ny] = true;
+    DFS(nx, ny, l + 1, sum + arr[nx][ny]);
+    if (l === 2) DFS(x, y, l + 1, sum + arr[nx][ny]);
+    isVisit[nx][ny] = false;
+  }
+};
+
+for (let i = 0; i < n; i++)
+  for (let j = 0; j < m; j++) {
+    isVisit[i][j] = true;
+
+    DFS(i, j, 1, arr[i][j]);
+    isVisit[i][j] = false;
+  }
 
 console.log(max);
 
