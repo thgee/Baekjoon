2969bb8b3751467f35d090142dddbce9bcfaf584
thgee <102576089+thgee@users.noreply.github.com>
2024-03-04 12:02:08 +0900

    Update 곱셈.js

--- 백준/Silver/1629. 곱셈/곱셈.js
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -1,21 +1,13 @@
-const fs = require("fs");
-let [A, B, C] = fs.readFileSync("/dev/stdin").toString().split(" ").map(BigInt);
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let arr = fs.readFileSync(filePath).toString().trim().split(" ");
 
-const solve = (power) => {
-  // B가 1이면 곱하나마나니까 바로 A%C 출력
-  if (power === 1n) {
-    return A % C;
-  }
-  // B를 2로 나눈 값을 입력하여 함수를 재귀적으로 호출한다.
-  const half = solve(power / 2n) % C;
+let [a, b, c] = arr.map((it) => BigInt(it));
 
-  // B를 2로 나눈 나머지가 1이면(홀수면) ((A^(B / 2) % C) * (A^(B / 2) % C) * (A % C)) % C 를 적용한다.
-  if (power % 2n) {
-    return (half * half * (A % C)) % C;
-  }
-
-  // B가 짝수면 ((A^(B / 2) % C) * (A^(B / 2) % C)) % C 를 적용한다.
-  return (half * half) % C;
+const DFS = (a, b) => {
+  if (b === 1n) return a % c;
+  let half = BigInt(DFS(a, b / 2n));
+  return b % 2n === 1n ? (half ** 2n * a) % c : half ** 2n % c;
 };
 
-console.log(solve(B).toString());
+console.log(parseInt(DFS(a, b) % c));
