8fbbd07b2fbdae71866f439c9c551a9bc9682fbe
thgee <102576089+thgee@users.noreply.github.com>
2024-03-06 16:39:49 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let arr = fs.readFileSync(filePath).toString().trim().split(" ");
+let [A, B, C] = arr.map((it) => Number(it));
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
