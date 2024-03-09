7454ccbd5a51ea46e43aac9b5be0eb234def0188
thgee <102576089+thgee@users.noreply.github.com>
2024-03-09 21:38:24 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let arr = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((it) => Number(it));
+console.log(arr[0] + arr[1]);
