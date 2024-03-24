f1cd192e39f2ea1f546d2f431b0ee333f2c3b9a9
thgee <102576089+thgee@users.noreply.github.com>
2024-03-24 21:52:16 +0900

    A＋B.js 업데이트

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,6 +1,6 @@
 let fs = require("fs");
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let arr = fs.readFileSync(filePath).toString().trim().split(" ");
+let arr = fs.readFileSync(filePath).toString().trim().split(" ")
 
 console.log(Number(arr[0]) + Number(arr[1]));
