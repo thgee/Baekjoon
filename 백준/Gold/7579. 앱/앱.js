e0139ba1de6fb7462d76dce10a5ae7a39c00671f
thgee <102576089+thgee@users.noreply.github.com>
2024-03-29 10:09:32 +0900

    [Gold III] Title: 앱, Time: 1796 ms, Memory: 168024 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.js
+++ 백준/Gold/7579. 앱/앱.js
@@ -1,7 +1,3 @@
-// 대각선당 하나의 비숍을 둘 수 있다.
-// 우상향 대각선으로 나눈 후에 각 대각선마다 가능한 칸에 비숍을 둔다.
-// 가능한 칸인지 판단하는 기준은 비숍을 두기 전에 좌상향 대각선에 다른 비숍이 있는지 확인한다.
-
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let arr = fs
