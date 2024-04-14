3d1fad493e675c7ca71c60c59c40a80302d3248a
thgee <102576089+thgee@users.noreply.github.com>
2024-04-14 17:01:29 +0900

    [Gold IV] Title: 바둑알 점프, Time: 124 ms, Memory: 9340 KB, Score: 60 point -BaekjoonHub

---
+++ 백준/Gold/17492. 바둑알 점프/바둑알 점프.js
@@ -0,0 +1,82 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let arr = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((it) =>
+    it
+      .trim()
+      .split(" ")
+      .map((it) => Number(it))
+  );
+
+let [N] = arr.shift();
+
+const offset = [
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+];
+let flag = false;
+let stoneCnt = 0;
+const getStoneNum = () => {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (arr[i][j] === 2) stoneCnt++;
+    }
+  }
+};
+
+const DFS = (cnt) => {
+  // 돌이 하나남으면 종료
+  if (cnt === 1) {
+    flag = true;
+    return;
+  }
+
+  // 벽 안쪽만 순회
+  for (let x = 1; x < N - 1; x++) {
+    for (let y = 1; y < N - 1; y++) {
+      // 바둑알을 만나면 해당 바둑알로부터 점프 시작
+
+      if (arr[x][y] === 2) {
+        for (let [dx, dy] of offset) {
+          // tx, ty는 밟고 넘어갈 바둑알 위치
+          let tx = x + dx,
+            ty = y + dy;
+          // nx, ny는 착지할 위치
+          let nx = x + dx * 2,
+            ny = y + dy * 2;
+
+          // 벽이면 점프못함
+          if (nx <= 0 || nx >= N || ny <= 0 || ny >= N - 1) continue;
+          // 밟은 돌 없으면 점프 못함
+          if (arr[tx][ty] !== 2) continue;
+
+          // 점프 !
+          arr[x][y] = 0;
+          arr[tx][ty] = 0;
+          arr[nx][ny] = 2;
+          DFS(cnt - 1);
+
+          // 백트래킹
+          arr[tx][ty] = 2;
+          arr[x][y] = 2;
+          arr[nx][ny] = 0;
+        }
+      }
+    }
+  }
+};
+
+getStoneNum();
+DFS(stoneCnt);
+
+console.log(flag ? "Possible" : "Impossible");
