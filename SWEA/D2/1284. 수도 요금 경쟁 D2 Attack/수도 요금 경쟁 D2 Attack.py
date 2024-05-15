378e4c9276cd968d5b5ea0111c36cefc42b3d5ff
thgee <102576089+thgee@users.noreply.github.com>
2024-05-15 12:55:48 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 108 ms, Memory: 42,768 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -0,0 +1,15 @@
+
+T = int(input())
+res = []
+for _ in range(T):
+  aCost,bBasicCost,bStdWater,bAddCost,useWater = map(int, input().split())
+
+  # a사와 b사의 요금을 계산해보고 작은 것을 res에 append
+  ac = aCost * useWater
+  bc =  bBasicCost + bAddCost * (useWater - bStdWater if useWater - bStdWater > 0 else 0)
+  res.append(ac if ac < bc else bc)
+
+
+
+
+print("\n".join([f"#{i + 1} {v}" for i, v in enumerate(res)]))
