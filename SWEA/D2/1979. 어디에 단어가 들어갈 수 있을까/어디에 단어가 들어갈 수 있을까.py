948a2eeed241057d9c821fdca113efccee2f13c4
thgee <102576089+thgee@users.noreply.github.com>
2024-05-15 12:56:48 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 121 ms, Memory: 46,744 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,5 +1,3 @@
-
-        
 def go_word(cnt, arr, level, x, y):
     tx, ty = x, y
     for dx, dy in offset: #  방향 잡고
@@ -8,42 +6,41 @@ def go_word(cnt, arr, level, x, y):
             for i in range(level): # level만큼 가보기
                 nx = x + dx
                 ny = y + dy
-
+ 
                 # 마지막 칸 체크
                 if level - 1 == i:
                     if  nx >= N or ny >= N:
                         cnt+=1
                         break
-                    
+                     
                     elif arr[nx][ny] == 0:
                         cnt+=1
                         break
-                    
+                     
                 # 마지막칸이 아닌 경우
                 else:
                     # 끝에 도달하면 종료
                     if  nx >= N or ny >= N: break
-
+ 
                     # 1을 찾으면 (길이 있으면)
                     if arr[nx][ny] == 1 :
                         x = nx
                         y = ny
                     else: break
     return cnt
-    
+     
 T = int(input())
 offset = [[1, 0], [0, 1]]
-
+ 
 for _ in range(T):
     cnt = 0
     arr = []
     N, K = list(map(int, input() .split()))
     for i in range(N):
         arr.append(list(map(int,input() .split())))
-    
+     
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 1:
                 cnt = go_word(cnt, arr,K,i, j)
     print(f"#{_ + 1} {cnt}")
-
