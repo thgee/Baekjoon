b2467a73d5f57753ce4a76a5045253375f34ea17
thgee <102576089+thgee@users.noreply.github.com>
2024-06-30 23:54:16 +0900

    [Bronze V] Title: A+B - 8, Time: 100 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((it) => it.split(" ").map((it) => Number(it)));
+
+const [N] = input.shift();
+
+res = [];
+
+for (let i = 0; i < N; i++)
+  res.push(
+    `Case #${i + 1}: ${input[i][0]} + ${input[i][1]} = ${input[i].reduce(
+      (acc, cur) => acc + cur
+    )}`
+  );
+
+console.log(res.join("\n"));
